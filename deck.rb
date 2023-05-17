9c8a0a67b403f2e8f5cda291bf9ec0f4d97e32bf
Konstantin Ulanov <GKondor@mail.ru>
2023-05-17 20:59:58 +0500

    Создал класс колода

---
+++ deck.rb
@@ -0,0 +1,27 @@
+class Deck
+  attr_accessor :card, :deck, :suit
+
+  SUIT = ["+", "<3", "^", "<>"]
+
+  def initialize
+    self.deck = []
+    SUIT.each do |s|
+      (1 .. 10).each do |i|
+        self.deck << [s, i]
+      end
+      ["J", "D", "K", "A"].each do |i| 
+        self.deck <<[s, i]  
+      end
+    end
+  end
+
+  def shuffle
+    deck.sort_by!{rand}
+  end
+
+  def take_card
+    i = deck.length
+    deck[]
+  end
+
+end
