ab99e361d9162c5190e9684170bae42b3dc4314b
KUlanov <GKondor@mail.ru>
2023-05-30 20:45:08 +0500

    Gamer initialize

--- deck.rb
+++ deck.rb
@@ -6,7 +6,7 @@ class Deck
   def initialize
     self.deck = []
     SUIT.each do |s|
-      (1 .. 10).each do |i|
+      (2 .. 10).each do |i|
         self.deck << [s, i]
       end
       ["J", "D", "K", "A"].each do |i| 
