58c65293cc4e45b9335dae126cca4f895d7a7fbd
KUlanov <GKondor@mail.ru>
2023-05-30 20:47:04 +0500

    Gamer initialize

---
+++ gamer.rb
@@ -0,0 +1,14 @@
+class Gamer
+  attr_accessor :card, :deck_g, :bank, :summ, :name
+
+  def initialize(name)
+    @name = name
+    @bank = 100
+    deck_g = []
+    summ = 0
+  end
+
+  def take_card(deck)
+    
+
+
