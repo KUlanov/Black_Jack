e7c16737049bc800f7a03eaa791c37c896ff3c22
KUlanov <GKondor@mail.ru>
2023-05-30 21:17:22 +0500

    30/05

--- gamer.rb
+++ gamer.rb
@@ -4,11 +4,16 @@ class Gamer
   def initialize(name)
     @name = name
     @bank = 100
-    deck_g = []
-    summ = 0
+    @deck_g = []
+    @summ = 0
   end
 
   def take_card(deck)
-    
+   deck_g << deck.pop
+  end
 
-
+  def summ_card
+    deck_g.each do |s, i|
+      if i.to_int == 
+      self.summ =
+  end
